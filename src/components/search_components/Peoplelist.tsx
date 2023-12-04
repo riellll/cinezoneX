2c4751f45690a9cb28aa7f484f7b4c81cea009a3
unknown <griel034@gmail.com>
2023-12-04 17:47:27 +0800

    Add Change

--- src/components/search_components/Peoplelist.tsx
+++ src/components/search_components/Peoplelist.tsx
@@ -5,7 +5,7 @@ const Peoplelist = ({people}: any) => {
   return (
     <div className="gap-5 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 mt-10 pb-5">
         {people.map((item: any,index: number) => (
-          <PeopleCards key={item.id} img={item.profile_path} name={item.name} id={item.id}/>
+          <PeopleCards key={item.id} img={item.profile_path} name={item.name} id={item.id} character=''/>
           ))}
       </div>
   )
