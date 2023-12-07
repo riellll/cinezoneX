d9c33e33fff8492de02f054c16450ba1afa7c7e3
unknown <griel034@gmail.com>
2023-12-07 13:56:24 +0800

    Add Change

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
 <footer className="relative flex bg-[url('/heroPic/oppenheimer.jpg')] bg-no-repeat bg-cover bg-center dark:bg-gray-900 mt-28">
-    <div className="absolute bg-black right-0 bottom-0 left-0 top-0 brightness-50 bg-opacity-90">dsd</div>
+    <div className="absolute bg-black right-0 bottom-0 left-0 top-0 brightness-50 bg-opacity-90"></div>
     {/* <Image src={'/heroPic/Creator.jpeg'} alt="img" width={500} height={500} className="absolute right-0 bottom-0 left-0 top-0 max-w-max h-fit saturate-70"/> */}
     <div className="flex flex-col mx-auto w-full max-w-screen-xl z-30">
       <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
@@ -75,7 +75,7 @@ const Footer = () => {
         </div>
     </div>
     <div className="px-4 py-6 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
+        <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://02portfolio.vercel.app/">Gabriel™</a>. All Rights Reserved.
         </span>
         <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
             <a href="#" className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
