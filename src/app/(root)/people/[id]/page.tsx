c7b66bc1d166a80d79cf1a618f97a834c0528007
unknown <griel034@gmail.com>
2023-12-15 23:54:35 +0800

    Add Change

---
+++ src/app/(root)/people/[id]/page.tsx
@@ -0,0 +1,61 @@
+import KnwnCards from "@/components/people_details/KnwnCards";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+const page = () => {
+
+  return (
+    <div className='flex flex-col lg:flex-row pt-28 px-5 text-gray-200 gap-7'>
+        <div className="flex flex-col items-center lg:items-start">
+        <img src="/jenn.jpg" alt="img" className='max-w-max max-h-52 md:max-h-64 lg:max-h-none rounded-lg'/>
+        <h1 className='lg:hidden text-4xl mt-8'>Jenna Ortega</h1>
+        <div className="flex gap-4 text-2xl mt-4 lg:mt-8">
+        <FaFacebook />
+        <FaTwitterSquare />
+        <FaInstagram />
+        <FaTiktok />
+        </div>
+        <div className=" mt-8">
+            <h1 className="text-xl pb-3">Personal Info</h1>
+            <div className="flex lg:flex-col flex-wrap gap-6 lg:gap-4">
+                <div>
+                    <h1 className="font-medium">Known For</h1>
+                    <p className="font-light">47</p>
+                </div>
+                <div>
+                    <h1 className="font-medium">Known Credits</h1>
+                    <p className="font-light">Acting</p>
+                </div>
+                <div>
+                    <h1 className="font-medium">Gender</h1>
+                    <p className="font-light">Female</p>
+                </div>
+                <div>
+                    <h1 className="font-medium">Birthday</h1>
+                    <p className="font-light">2002-09-27 (21 years old)</p>
+                </div>
+                <div>
+                    <h1 className="font-medium">Place of Birth</h1>
+                    <p className="font-light">Coachella Valley, Palm Desert, California, USA</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className='flex flex-col gap-10'>
+            <h1 className='hidden lg:block text-4xl'>Jenna Ortega</h1>
+            <div className='flex flex-col gap-3 max-w-[950px] xl:max-w-[950px] lg:max-w-[700px]'>
+                <h1 className='text-xl'>Biography</h1>
+                <div className="whitespace-pre-line ...">
+                Jenna Marie Ortega  born September 27, 2002  is an American actress. She began her career as a child actress, receiving recognition for her role as young Jane on The CW comedy-drama series Jane the Virgin  2014 2019 . She had her breakthrough for starring as Harley Diaz on the Disney Channel series Stuck in the Middle  2016 2018 , for which she won an Imagen Award. She played Ellie Alves in the second season of the Netflix thriller series You in 2019 and starred in the Netflix family film Yes Day  2021 .<br/><br/>Ortega received critical praise for her performance in the teen drama The Fallout  2021 , and went on to star in the slasher films X  2022 , Scream  2022  and its sequel Scream VI  2023 , establishing herself as a scream queen. She starred as Wednesday Addams in the Netflix horror comedy series Wednesday 2022, for which she received nominations for a Primetime Emmy Award, a Golden Globe Award and a SAG Award.<br/><br/>Description above from the Wikipedia article Jenna Ortega, licensed under CC BY SA, full list of contributors on Wikipedia.
+                </div>
+            </div>
+            <div className="flex flex-col">
+            <KnwnCards/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/people_details/KnwnCards.tsx
@@ -0,0 +1,61 @@
+import React, { Suspense } from 'react'
+import {ScrollShadow} from "@nextui-org/react";
+import GetTrending from '@/lib/FetchTrending';
+import Link from 'next/link';
+import { MdOutlinePlayCircleFilled } from 'react-icons/md';
+import {Image} from "@nextui-org/react";
+const KnwnCards = async () => {
+    const {results} = await GetTrending('day')
+  return (
+    <>
+    <h1 className="mb-4 text-xl font-bold leading-none tracking-tight text-gray-300 md:text-lg lg:text-2xl dark:text-white">Recommendations</h1>
+    <ScrollShadow orientation="horizontal" className="flex gap-5 max-w-[950px] xl:max-w-[950px] lg:max-w-[700px]">
+    {/* <div className="flex gap-5"> */}
+        {results.map((item: any) =>{ 
+        const link = item.media_type === 'tv' ? `/tv/details/${item.id}` : `/movie/details/${item.id}`
+        return(
+           <div
+           className="relative group cards selection: max-w-full min-h-full rounded-lg dark:bg-gray-800 dark:border-gray-700"
+           key={item.id}
+         >
+           <Link href={link} className="relative imgcards">
+           {/* <span className="transition ease-in-out delay-150 duration-700 absolute rounded-md top-0 bottom-0 left-0 right-0"><MdOutlinePlayCircleFilled /></span> */}
+           <Suspense fallback={<h1 className="w-full bg-red-600 text-gray-200">Loading</h1>}>
+             <Image
+               className="rounded-lg max-w-[150px] h-auto shadow-inner"
+               src={`https://image.tmdb.org/t/p/w220_and_h330_face${item.poster_path}`}
+               alt={item.title || item.name}
+               width={500}
+               height={500}
+             />
+               </Suspense>
+             <span className="transition ease-in-out delay-300 duration-1000 absolute text-6xl bgcard rounded-md top-0 bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 flex justify-center items-center z-20"><MdOutlinePlayCircleFilled /></span>
+           </Link>
+           <div className="sm:p-1 min-[320px]:p-2">
+{/*              <div className="flex justify-between text-gray-400 text-xs">
+             <p className=" font-norma dark:text-gray-400">
+               {datee}
+             </p>
+             <p className="transition ease-in-out delay-150 duration-700 font-norma dark:text-gray-400 border text-center border-gray-400 px-1.5 rounded-full group-hover:text-green-600 group-hover:border-green-600">
+               {media === 'tv' ? 'SS 1' : 'MOVIE'}
+             </p>
+             <div className="flex items-center font-norma dark:text-gray-400">
+               <p><IoMdStar /></p>
+               <p>{votee}</p>
+             </div>
+             </div> */}
+             <Link href={link}>
+               <h5 className="mb-2 sm:text-xs font-normal sm:font-semibold text-gray-100 dark:text-gray-300 text-sm group-hover:text-green-600 transition ease-in-out delay-150 duration-700">
+                 {item.title || item.name}
+               </h5>
+             </Link>
+           </div>
+         </div>
+        )})}
+      {/* </div> */}
+      </ScrollShadow>
+      </>
+  )
+}
+
+export default KnwnCards
